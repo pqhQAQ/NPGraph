d03f2d1b2a322eb4a28839762d28dd3f61e9fcb0
pqhQAQ <534154960@qq.com>
2018-06-12 10:44:38 +0800

    add test

--- src/client/IntraGenerator.java
+++ src/client/IntraGenerator.java
@@ -11,27 +11,41 @@ import soot.Transform;
 import soot.options.Options;
 
 public class IntraGenerator {
-	//generator
+	// generator
 	public static int Generator_ID = 0;
-	
-	
-	public static void main(String[] args) {
-		//parse arguments
-		//TODO
 
-		String sp = "D:/project/workspace/NPGraph/"; 
-        String path =sp + "bin" + File.pathSeparator + sp + "lib/soot-2.5.0.jar" + File.pathSeparator +sp + "lib/sootsources-trunk.jar"+ File.pathSeparator + sp + "lib/rt.jar";
-        Scene.v().setSootClassPath(path);
-		
-		//set options
+	public static void main(String[] args) {
+		// parse arguments
+		// TODO
+
+		String sp = "D:/project/workspace/NPGraph/lib/";
+		//String path = sp + "bin" + File.pathSeparator + sp + "lib/soot-2.5.0.jar" + File.pathSeparator + sp
+		//		+ "lib/sootsources-trunk.jar" + File.pathSeparator + sp + "lib/rt.jar" + File.pathSeparator + sp
+		//		+ "lib/test" + File.pathSeparator + sp + "lib/xmlgraphics-commons-1.3.1.jar";
+		File dirFile = new File(sp);
+		String path = "";
+		if (!dirFile.isDirectory()) {   
+            return ;  
+        }
+		String[] fileList = dirFile.list();
+		for (int i = 0; i < fileList.length; i++) {  
+            //遍历文件目录  
+            String string = fileList[i];  
+            if(string.endsWith(".jar")){
+            	path  += sp+string+";";
+            }
+		}
+		path += sp+"test;";
+		Scene.v().setSootClassPath(path);
+
+		// set options
 		Options.v().setPhaseOption("jb", "use-original-names:true");
 		Options.v().setPhaseOption("tag", "off");
 		Options.v().set_output_format(Options.output_format_jimple);
 		Options.v().set_keep_line_number(true);
 		Options.v().set_prepend_classpath(true);
-		
-		
-		//add phase
+
+		// add phase
 		Transform trans = null;
 		switch (Generator_ID) {
 		case 0:
@@ -39,7 +53,7 @@ public class IntraGenerator {
 			trans = new Transform("jtp.peggenerator", peggenerator);
 			break;
 		case 1:
-			
+
 			break;
 		case 2:
 			break;
@@ -47,19 +61,18 @@ public class IntraGenerator {
 			System.err.println("wrong generator!!!");
 			System.exit(0);
 		}
-		
+
 		PackManager.v().getPack("jtp").add(trans);
-		
-		
-		//run 
+
+		// run
 		soot.Main.main(args);
 		PEGGenerator peg = new PEGGenerator();
 		peg.createCall();
-		//List<SootMethod> am = peg.allMethod;
-		//for(SootMethod method : am){
-			//peg.createIntra();
-		//}
-		
+		// List<SootMethod> am = peg.allMethod;
+		// for(SootMethod method : am){
+		// peg.createIntra();
+		// }
+
 	}
 
 }
